29d55c85fdad68ee5502a06db29a3eb73c6abaa3
philip <smithp1992@gmail.com>
2023-08-09 16:08:38 -0400

    Added default landing page for trading_bot. - Added port exposure for docker server. - Updated express to work with public folder.

--- express.js
+++ express.js
@@ -1,11 +1,14 @@
+const path = require('path');
 const express = require('express');
-const app = express();
 
 let lib = require('./lib');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(process.env['PORT'] || 3000, () => {
   console.log(`Example app listening on port ${process.env['PORT'] || 3000}`)
